d95fdfdf4200fe6109b209569d7d9c14db8841cd
Leon-Salenbacher <leon@salenbacher.com>
2022-12-03 15:08:16 +0100

    day 3 part 2

--- 3.day/main.py
+++ 3.day/main.py
@@ -30,6 +30,16 @@ def getPrio(listOfItems):
 
     return total_prio
 
+def find_group_attachement(first, second, third):
+    for i in first:
+        for e in second:
+            for a in third:
+                if(i == e and e == a):
+                    return i
+
+
+
+
 
 def part1():
     f = open("./3.day/input.txt", "r")
@@ -41,7 +51,29 @@ def part1():
     total_prio = getPrio(list_ofDobleItems)
     print(total_prio)
 
-
+def part2():
+    f = open("./3.day/input.txt", "r")
+    
+    index = 0
+    first = []
+    second = []
+    third = []
+    all_Attachemnts = []
+    for l in f:
+        # get 3 
+        if(index == 0):
+            first = list(l)
+            index=1
+        elif(index == 1):
+            second = list(l)
+            index=2
+        elif(index == 2):
+            third = list(l)
+            all_Attachemnts.append(find_group_attachement(first, second, third))
+            index = 0
+    
+    total_prio = getPrio(all_Attachemnts)
+    print(total_prio)
 
 if '__main__' == __name__:
-    part1()
+    part2()
